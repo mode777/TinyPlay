726a9a84d36f03a36f42e83f2a9e9c0cf65092b6
Alexander Klingenbeck (SHS DI SY R&D DEV4) <alexander.klingenbeck@siemens-healthineers.com>
2023-04-19 10:48:16 +0200

    Fix missing cast to void*

--- src/main.c
+++ src/main.c
@@ -438,7 +438,7 @@ int main(int argc, char* argv[]) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Error evaluating JavaScript: %s", duk_safe_to_string(ctx, -1));
         return 1;
     }
-    free(buf);
+    free((void*)buf);
 
     // Find the draw() function in the global object
     duk_get_global_string(ctx, "draw");
